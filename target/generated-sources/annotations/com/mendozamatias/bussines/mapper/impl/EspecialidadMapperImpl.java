5bea9bccab98edcd8f649dc159a3d795b1a11c83
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-05-15 11:03:06 -0300

    modificacion en docker file y application.properties

--- target/generated-sources/annotations/com/mendozamatias/bussines/mapper/impl/EspecialidadMapperImpl.java
+++ target/generated-sources/annotations/com/mendozamatias/bussines/mapper/impl/EspecialidadMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-15T09:40:09-0300",
+    date = "2024-05-15T11:00:31-0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
